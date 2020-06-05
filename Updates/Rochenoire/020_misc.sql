65732bce7ce46a297bd95f7a04319751ef8445f1
Lesueur Benjamin <valkiriebc@hotmail.fr>
2020-06-05 18:29:25 +0200

    fixed encoding on misc

--- Updates/Rochenoire/020_misc.sql
+++ Updates/Rochenoire/020_misc.sql
@@ -1,3 +1,6 @@
+SET CHARSET 'utf8';
+set names utf8;
+
 -- Apprentice Riding (Requires level 20)
 UPDATE mail_level_reward SET level = 20 WHERE level = 30 AND mailTemplateId IN (233, 232, 231, 230, 229, 228, 227, 226, 225, 224);
 
