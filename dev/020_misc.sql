1f95adebf36d2a24582ab556963fbf2dc42ac8f0
Lesueur Benjamin <valkiriebc@hotmail.fr>
2021-01-11 13:02:25 +0100

    add missing quest scaling for Paladin/Warlock mounts at level 20

--- dev/020_misc.sql
+++ dev/020_misc.sql
@@ -4,8 +4,8 @@ set names utf8;
 -- Apprentice Riding (Requires level 20)
 UPDATE mail_level_reward SET level = 20 WHERE level = 30 AND mailTemplateId IN (233, 232, 231, 230, 229, 228, 227, 226, 225, 224);
 
--- Paladin/Warlock Mounts available at level 20
--- UPDATE `quest_template` SET `MinLevel` = 20, `QuestLevel` = 20 WHERE `entry` IN (1661,3631,4485,4486,4487,4488,4489,4490);
+-- Paladin/Warlock 60% Mounts available at level 20
+UPDATE `quest_template` SET `MinLevel` = 20 WHERE `entry` IN (1661, 3631, 4485, 4486, 4487, 4488, 4489, 4490);
 UPDATE npc_trainer_template SET reqlevel = 20 WHERE spell IN (1710, 5784, 5785, 13819, 34769, 34768, 13820, 13819);
 
 -- Flexible Raid text implementation
